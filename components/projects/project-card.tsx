71e77ae17d4bdd97694d368cd52904dcab25707f
NecroBread <mariovyordanov@gmail.com>
2022-09-17 17:18:39 +0300

    create: project card, types, resources

---
+++ components/projects/project-card.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+import { TProject } from '../../types/types'
+
+const ProjectCard: React.FC<{ project: TProject, reverse: boolean }> = ({ project, reverse }) => {
+    return (
+        <div className={`flex flex-col gap-4  ${reverse ? 'md:flex-row-reverse' : 'md:flex-row'}`}>
+            <div className=''>
+                <Image className={`rounded-xl border-2 border-primary ${reverse ? 'project-img-reversed' : 'project-img'}`} src={project.img} width={600} height={350} alt="screenshot of social media app" />
+            </div>
+            <div className={`flex items-center max-w-3xl p-4`}>
+                <div className={`flex flex-col ${reverse ? 'items-end' : 'items-start'}`}>
+                    <div className=' bg-accent rounded-md relative w-fit p-4 z-20 mb-2'>
+                        <div className='bg-base border-2 rounded-md border-accent absolute w-full h-full -left-1 -top-1 -z-20'></div>
+                        <h3 className='text-xl font-black z-30 uppercase -translate-x-1 -translate-y-1'>{project.title}</h3>
+                    </div>
+                    <div className='px-4 py-2'>
+                        <p>{project.description}</p>
+                    </div>
+                    <div className='px-4 py-2 flex gap-4'>
+                        <div>
+                            <a href={project.links.demo} className='text-primary font-bold hover:text-primary-focus fill-primary hover:fill-primary-focus'>Demo</a>
+                        </div>
+                        <div>
+                            <a href={project.links.github} className='text-primary font-bold hover:text-primary-focus fill-primary hover:fill-primary-focus'>GitHub</a>
+                        </div>
+                    </div>
+                    <div className='px-4 py-2 flex gap-2'>
+                        {project.tech.map(x => <div key={x} className='p-2 bg-slate-500 h-fit w-fit rounded-md'>{x}</div>)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
--- types/types.ts
+++ types/types.ts
@@ -2,3 +2,14 @@ export type TSkill = {
     skillName: string,
     classes: string,
 }
+
+export type TProject = {
+    title: string,
+    img: string,
+    description: string,
+    links: {
+        demo: string,
+        github: string
+    },
+    tech: string[]
+}
