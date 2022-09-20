2ebe1347a07fec65d1789c8f7380d3f5b4756889
NecroBread <mariovyordanov@gmail.com>
2022-09-20 22:06:18 +0300

    add: title to portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
-import type { NextPage } from 'next'
-import About from '../components/about-me/about-me'
-import Contact from '../components/contact/contact'
-import Hero from '../components/hero/hero'
-import Submenu from '../components/submenu/submenu'
-import { Projects } from '../components/projects/projects'
+import type { NextPage } from 'next';
+import About from '../components/about-me/about-me';
+import Contact from '../components/contact/contact';
+import Hero from '../components/hero/hero';
+import Submenu from '../components/submenu/submenu';
+import { Projects } from '../components/projects/projects';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
     return (
         <>
+            <Head>
+                <title>Mario Yordanov</title>
+            </Head>
             <Hero />
             <Submenu />
             <About />
