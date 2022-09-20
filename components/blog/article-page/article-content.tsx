9e2b1a0bff9d79eaa6e6faf0957aa445738122ef
NecroBread <mariovyordanov@gmail.com>
2022-09-20 14:56:26 +0300

    add: SyntaxHighlighter

--- components/blog/article-page/article-content.tsx
+++ components/blog/article-page/article-content.tsx
@@ -1,9 +1,37 @@
-import ReactMarkdown, { Options, Components } from "react-markdown";
+/* eslint-disable react/no-children-prop */
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 const ArticleContent: React.FC<{ content: string }> = ({ content }) => {
     return (
         <div className="prose prose-invert max-w-none font-serif prose-headings:font-mono">
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown
+                // eslint-disable-next-line react/no-children-prop
+                children={content}
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, '')}
+                                // @ts-ignore
+                                style={a11yDark}
+                                // @ts-ignore
+                                customStyle={{ backgroundColor: 'transparent' }}
+                                showLineNumbers={true}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }}
+            />,
         </div>
     )
 }
