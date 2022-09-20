23aa474d19b54846777c04204af439c26e5bd468
NecroBread <mariovyordanov@gmail.com>
2022-09-20 13:03:09 +0300

    add: tailwind typography

--- components/blog/article-page/article-content.tsx
+++ components/blog/article-page/article-content.tsx
@@ -2,7 +2,7 @@ import ReactMarkdown, { Options, Components } from "react-markdown";
 
 const ArticleContent: React.FC<{ content: string }> = ({ content }) => {
     return (
-        <div className="p-4">
+        <div className="prose prose-invert p-4 max-w-none">
             <ReactMarkdown>{content}</ReactMarkdown>
         </div>
     )
