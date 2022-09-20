0beae6dd864d416bcc9621131c08dd999338a6ff
NecroBread <mariovyordanov@gmail.com>
2022-09-20 12:39:22 +0300

    add: load md file into blog

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,29 +1,52 @@
-import Link from "next/link";
-import ArticleDetails from "../../components/blog/article-page/article-details";
+import { GetStaticProps } from "next";
+import ArticleContent from "../../components/blog/article-page/article-content";
 import ArticleFooter from "../../components/blog/article-page/article-footer";
+import ArticleHeader from "../../components/blog/article-page/article-header";
+import { getArticleData, getArticlesFiles } from "../../helpers/articles-utils";
 import { TArticle } from "../../types/types";
 
-const dummy: TArticle = {
-    title: 'Hello World',
-    tags: ['TypeScript'],
-    content: 'First Post in the Blog',
-    date: '2022-12-12',
-    excerpt: 'TLDR: First',
-    slug: '# hello-world'
-}
-
-
-const BlogArticle = () => {
+const BlogArticle: React.FC<{ article: TArticle }> = ({ article }) => {
     return (
         <div>
             <section className="mx-auto max-w-3xl">
-                <ArticleDetails article={dummy} />
+                <article className='my-12'>
+                    <ArticleHeader article={article} />
+                    <ArticleContent content={article.content} />
+                </article>
             </section>
             <section className="mx-auto max-w-3xl">
                 <ArticleFooter />
             </section>
         </div>
     )
+}
+export const getStaticProps: GetStaticProps = (context) => {
+    console.log(context);
+
+    const { params } = context;
+    const { slug } = params as { slug: string };
+
+    const article = getArticleData(slug);
+
+    return {
+        props: {
+            article,
+        },
+        revalidate: 600,
+    }
+}
+
+export const getStaticPaths = () => {
+    const articlesFiles = getArticlesFiles();
+    const slugs = articlesFiles.map(x => x.replace(/\.md$/, ''));
+    return {
+        paths: slugs.map(slug => ({
+            params: {
+                slug,
+            }
+        })),
+        fallback: false,
+    }
 }
 
 export default BlogArticle;
