e20748611ae42f678ab3ad0ab14d817f10233c34
NecroBread <mariovyordanov@gmail.com>
2022-09-23 10:01:34 +0300

    add: link outline component

---
+++ components/common/link-btn-outline.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React, { ReactNode } from 'react'
+
+const LinkBtnOutline: React.FC<{ children: ReactNode }> = ({ children }) => {
+    return (
+        <Link href="/" >
+            <a className="p-4 border-2 border-primary rounded-md text-primary hover:bg-primary hover:text-base active:text-sm flex w-fit gap-3 hover:fill-base fill-primary items-center">
+                {children}
+            </a>
+        </Link>
+    )
+}
+
+export default LinkBtnOutline
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
+import LinkBtnOutline from '../components/common/link-btn-outline';
 
 const custom404 = () => {
     return (
         <div className='w-full min-h-[calc(100vh-232px)] grid grid-cols-1 md:grid-cols-2 mb-52 md:mb-0'>
             <div className='p-4 flex flex-col items-center md:items-end justify-center'>
                 <h1 className='font-bold text-3xl uppercase mb-8'>404 | Lost in Space?</h1>
-                <Link href="/" >
-                    <a className="p-4 border-2 border-primary rounded-md text-primary hover:bg-primary hover:text-base active:text-sm flex w-fit gap-3 hover:fill-base fill-primary items-center">
-                        <div>
-                            <svg width="25" height="25" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" /></svg>
-                        </div>
-                        Go Home
-                    </a>
-                </Link>
+                <LinkBtnOutline>
+                    <div>
+                        <svg width="25" height="25" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm3 5.753l-6.44 5.247 6.44 5.263-.678.737-7.322-6 7.335-6 .665.753z" /></svg>
+                    </div>
+                    Go Home
+                </LinkBtnOutline>
             </div>
             <div className='flex justify-center items-center relative'>
                 <div className='relative'>
